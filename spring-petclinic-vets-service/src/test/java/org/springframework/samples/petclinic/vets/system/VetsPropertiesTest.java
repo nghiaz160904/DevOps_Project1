e6beb32617e382f47fe4a6e78b09b585256498ba
Hồ Phước Nghĩa <118056302+nghiaz160904@users.noreply.github.com>
2025-03-15 20:45:52 +0700

    Update VetsPropertiesTest.java

--- spring-petclinic-vets-service/src/test/java/org/springframework/samples/petclinic/vets/system/VetsPropertiesTest.java
+++ spring-petclinic-vets-service/src/test/java/org/springframework/samples/petclinic/vets/system/VetsPropertiesTest.java
@@ -1,14 +1,21 @@
 package org.springframework.samples.petclinic.vets.system;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.runner.ApplicationContextRunner;
-import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@EnableConfigurationProperties(VetsProperties.class)
+@TestPropertySource(properties = {
+    "spring.profiles.active=production",
+    "vets.cache.ttl=3600",
+    "vets.cache.heapSize=100"
+})
 class VetsPropertiesTest {
 
     @Autowired
@@ -16,14 +23,9 @@ class VetsPropertiesTest {
 
     @Test
     void testVetsPropertiesLoaded() {
-        assertNotNull(vetsProperties);
-        assertNotNull(vetsProperties.cache());
+        assertNotNull(vetsProperties, "VetsProperties should not be null");
+        assertNotNull(vetsProperties.cache(), "Cache should not be null");
         assertEquals(3600, vetsProperties.cache().ttl(), "TTL should be 3600");
         assertEquals(100, vetsProperties.cache().heapSize(), "Heap size should be 100");
-    }
-
-    @TestConfiguration
-    @EnableConfigurationProperties(VetsProperties.class)
-    static class Config {
     }
 }
