35781252696eea40fc141dff0bdca27c331d98ad
Hồ Phước Nghĩa <118056302+nghiaz160904@users.noreply.github.com>
2025-03-16 21:45:36 +0700

    Update VisitRepository.java

--- spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/model/VisitRepository.java
+++ spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/model/VisitRepository.java
@@ -35,5 +35,5 @@ public interface VisitRepository extends JpaRepository<Visit, Integer> {
     List<Visit> findByPetId(int petId);
 
     List<Visit> findByPetIdIn(Collection<Integer> petIds);
-    //hello nghiax
+    //hello nghiaxz
 }
